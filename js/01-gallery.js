9b930bdd10c4ae7d67ecafa61741705f8503ac62
Kateryna Harmash <139209869+KaterynaHarmash@users.noreply.github.com>
2023-11-19 19:51:02 +0200

    first gallery added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('ul.gallery');
+let targetItem = ``;
 
-console.log(galleryItems);
+createGallery(galleryItems);
+
+galleryRef.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(evt) {
+    evt.preventDefault();
+    if (evt.target.classList.contains('gallery__image')) {
+        callModalBox(evt.target);
+    }
+}
+ function callModalBox(target) {
+    targetItem = `<img src="${target.dataset.source}" width="800" height="600" alt="${target.alt}">`;
+    return basicLightbox.create(targetItem).show();
+}
+function createGallery(items) {
+    const galleryListMarkup = items.map(({preview, original, description}) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a>
+    </li>`
+    });
+
+    galleryRef.insertAdjacentHTML('beforeend', galleryListMarkup.join(''));
+}
