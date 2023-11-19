d570b3dac63926883d706d54cd7669f8643b95c6
Kateryna Harmash <139209869+KaterynaHarmash@users.noreply.github.com>
2023-11-19 20:42:22 +0200

    2 gallery added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,20 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryRef = document.querySelector('ul.gallery');
 let targetItem = ``;
-
+let instance;
 createGallery(galleryItems);
 
 galleryRef.addEventListener('click', onGalleryItemClick);
+galleryRef.addEventListener('keydown', (evt) => {
+    if (instance.visible()&&evt.key==='Escape') {
+        instance.close();
+    }
+    
+});
 
 function onGalleryItemClick(evt) {
     evt.preventDefault();
     if (evt.target.classList.contains('gallery__image')) {
         callModalBox(evt.target);
+        instance.show();
     }
 }
  function callModalBox(target) {
-    targetItem = `<img src="${target.dataset.source}" width="800" height="600" alt="${target.alt}">`;
-    return basicLightbox.create(targetItem).show();
+     targetItem = `<img src="${target.dataset.source}" width="800" height="600" alt="${target.alt}">`;
+     instance = basicLightbox.create(targetItem);
+     return instance;
 }
 function createGallery(items) {
     const galleryListMarkup = items.map(({preview, original, description}) => {
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('ul.gallery');
+createGallery(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: 'true',
+    captionSelector: '.gallery img',
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+});
 
-console.log(galleryItems);
+function createGallery(items) {
+    const galleryListMarkup = items.map(({preview, original, description}) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    });
+
+    galleryRef.insertAdjacentHTML('beforeend', galleryListMarkup.join(''));
+}
